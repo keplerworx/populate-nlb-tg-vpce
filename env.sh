1feff63877f632c2324b0f1e31414dc2d03f036a
Naveed ur Rahman <navidurrahman@gmail.com>
2020-03-02 12:56:38 +0400

    Adding code files

---
+++ env.sh
@@ -0,0 +1,8 @@
+export DNS_NAME=vpce-02b6d65398b279296-7k61a5o9.storagegateway.me-south-1.vpce.amazonaws.com
+export DNS_LISTENERS=443,1026,1027,1028,1031,2222
+export S3_BUCKET=test-empty-bucket-n
+export NLB_TG_ARN=arn:aws:elasticloadbalancing:me-south-1:807410046616:targetgroup/sg-tg/cb5e04475a57e62a
+export MAX_LOOKUP_PER_INVOCATION=50
+export INVOCATIONS_BEFORE_DEREGISTRATION=3
+export CW_METRIC_FLAG_IP_COUNT=true
+
